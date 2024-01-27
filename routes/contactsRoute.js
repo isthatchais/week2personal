37a5cf048799805cc9f42ebbd33f53611cd89ba7
Chais Chang <cha96001@byui.edu>
2024-01-27 16:21:46 -0700

    worling locally

--- routes/contactsRoute.js
+++ routes/contactsRoute.js
@@ -1,45 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const mongodb = require('../db/connect');
-const ObjectId = require('mongodb').ObjectId
 
-router.get("/", async (req, res) => {
-    let collection = await mongodb.getDb().db().collection("contacts");
-    let results = await collection.find({})
-      .toArray();
-    res.send(results).status(200);
-});
+const contactsController = require('../controllers/contactsController');
 
+router.get('/', contactsController.getAllContacts);
 
-router.get("/:id", async (req, res) => {
-  let collection = await mongodb.getDb().db().collection("contacts");
-  let id = new ObjectId(req.params.id);
-  let result = await collection.find({_id: id}).toArray();
-  if (!result) res.send("Not found").status(404);
-  else res.send(result).status(200);
-});
+router.get('/:id', contactsController.getOneContact);
 
+router.post('/addContact', contactsController.addContact);
 
-/*
-router.get("/:id", async (req, res) => {
-    let collection = await mongodb.getDb().db().collection("contacts");
-    let results = await collection.find({_id: ObjectId(req.params.id)}).toArray();
-    res.send(results).status(200);
-});
+router.put('/updateContact/:id', contactsController.updateContact);
 
+router.delete('/deleteContact/:id', contactsController.deleteContact);
 
-db.vehicle.find(
-{
-    "_id": {
-        $in: [
-            ObjectId("62d01d17cdd1b7c8a5f945b9"),
-            ObjectId("62d01d17cdd1b7c8a5f945ba"),
-            ObjectId("62d01d17cdd1b7c8a5f945bb")
-        ]
-    }
-});
-
-
-*/
-
-  module.exports = router;
+module.exports = router;
--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
-const MongoClient = require('mongodb').MongoClient;
+//const MongoClient = require('mongodb').MongoClient;
 const mongodb = require('./db/connect');
 const app = express();
 const port = process.env.PORT || 8080;
@@ -8,17 +8,17 @@ const port = process.env.PORT || 8080;
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', '*');
-    next()
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  next();
 });
 
 app.use('/', require('./routes'));
 
-mongodb.initDb((err, mongodb) => {
-    if (err) {
-      console.log(err);
-    } else {
-      app.listen(port);
-      console.log(`Connected to DB and listening on ${port}`);
-    }
-  });
+mongodb.initDb((err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    app.listen(port);
+    console.log(`Connected to DB and listening on ${port}`);
+  }
+});
