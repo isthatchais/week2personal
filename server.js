21a4931d97a8db03862f2cff3be0f035afef1b60
Chais Chang <cha96001@byui.edu>
2024-02-03 20:03:28 -0700

    updates for react frontend

--- server.js
+++ server.js
@@ -9,6 +9,12 @@ app.use(bodyParser.json());
 
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-with, Content-Type, Accept, Z-Key'
+  );
+  res.setHeader('Content_Type', 'application/json');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
   next();
 });
 
